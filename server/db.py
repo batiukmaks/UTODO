c10d2fad12ca4abdc26bc7b3f3b099f4ce786c8c
batiukmaks <batiukmaks@gmail.com>
2023-04-26 10:21:10 +0300

    Solve max_pool problem

--- server/db.py
+++ server/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql://root:mysql007@127.0.0.1:3306/utodolist?charset=utf8mb4&multi_statements=True', pool_pre_ping=True)
+engine = create_engine('mysql://root:mysql007@127.0.0.1:3306/utodolist?charset=utf8mb4&multi_statements=True', pool_pre_ping=True, pool_size=50)
 Session = sessionmaker(bind=engine)
 
 def get_session():
